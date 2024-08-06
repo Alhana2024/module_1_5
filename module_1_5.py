6f2b256634facda0b028f734f1a7259b1c79c70c
Alhana2024 <alhana@mail.ru>
2024-08-06 16:41:44 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = tuple([5, 4, False, 6.1, 'saddle'])
+print('Immutable tuple:',immutable_var)
+#immutable_var [0] = 10
+#print('Immutable tuple:',immutable_var)
+# При попытке изменения элементов программа выдает TypeError: 'tuple' object does not support item assignment
+# Так как программа не поддерживает обращение непосредственно к элементам кортежа, кортеж неизменен)
+mutable_list = [5, 4, False, 6.1, 'saddle']
+mutable_list[4] = ('horse')
+mutable_list.append('hat')
+print('Mutable list:', mutable_list)
